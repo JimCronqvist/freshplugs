b48cc8ac49851ebe42cd2d9be2d18f58af6e689f
Jim Cronqvist <jim.cronqvist@gmail.com>
2015-07-15 23:21:09 +0200

    Create Ticket_View_List_Count.js

---
+++ Ticket_View_List_Count.js
@@ -0,0 +1,38 @@
+jQuery(function()
+{
+    jQuery('#active_filter').on('click', function()
+    {
+        jc_old_cookie = getCookie('filter_name');
+        jQuery('.fd-menu').children('a').slice(0,3).each(function()
+        {
+            var $this = jQuery(this);
+            if($this.attr('data-jc_counted') == (new Date()).getMinutes())
+            {
+                return true;
+            }
+            var href = $this.attr('href');
+            if(href.indexOf('/helpdesk/tickets/view/') != -1)
+            {
+                var viewId = href.replace('/helpdesk/tickets/view/', '');
+                console.log(viewId);
+                document.cookie = "filter_name="+viewId+"; expires=0; path=/";
+                jQuery.ajax({
+                    url: '/helpdesk/tickets/view/'+viewId+'?_pjax=%23body-container',
+                    success: function(data)
+                    {
+                        try{
+                            var number = data.match(/Showing([\s\S]+?)of/)[0].match(/([0-9]+)/g)[1];
+                        } catch(exception){
+                            var number = 0;
+                        }
+                        console.log(viewId+': '+number);
+                        $this.children('span').remove();
+                        $this.html($this.text()+'<span class="badge" style="float:right;">'+number+'</span>');
+                        $this.attr('data-jc_counted', (new Date()).getMinutes());
+                        document.cookie = "filter_name="+jc_old_cookie+"; expires=0; path=/";
+                    }
+                });
+            }
+        });
+    });
+});
