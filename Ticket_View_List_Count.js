974e477074919b587e48819109697a09be9b092f
Jim Cronqvist <jim.cronqvist@gmail.com>
2015-07-16 00:52:12 +0200

    Update Ticket_View_List_Count.js

--- Ticket_View_List_Count.js
+++ Ticket_View_List_Count.js
@@ -1,6 +1,10 @@
 jQuery(function()
 {
     jc_view_count = {};
+    var allowed_global_views = [
+        '/helpdesk/tickets/filter/spam'
+    ];
+
     jQuery(document).on('click', '#active_filter', function()
     {
         var jc_old_cookie = getCookie('filter_name');
@@ -8,9 +12,17 @@ jQuery(function()
         {
             var $this = jQuery(this);
             var href = $this.attr('href');
-            if(href.indexOf('/helpdesk/tickets/view/') != -1)
+            if(href.indexOf('/helpdesk/tickets/view/') != -1 || allowed_global_views.indexOf(href) != -1)
             {
-                var viewId = href.replace('/helpdesk/tickets/view/', '');
+                var viewId = href.replace('/helpdesk/tickets/view/', '').replace('/helpdesk/tickets/filter/', '');
+                if(href.indexOf('/helpdesk/tickets/view/') != -1)
+                {
+                    var baseUrl = '/helpdesk/tickets/view/';
+                }
+                else
+                {
+                    var baseUrl = '/helpdesk/tickets/filter/';
+                }
                 if(typeof jc_view_count[viewId] == 'undefined')
                 {
                     jc_view_count[viewId] = {minute: null, number: 0};
@@ -22,7 +34,7 @@ jQuery(function()
                 }
                 jQuery.ajax(
                 {
-                    url: '/helpdesk/tickets/view/'+viewId+'?_pjax=%23body-container',
+                    url: baseUrl+viewId+'?_pjax=%23body-container',
                     success: function(data)
                     {
                         try{
