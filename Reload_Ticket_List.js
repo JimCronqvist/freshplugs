b0e7708cf279b5562cebccb3ae6f6b3557dbaf0c
Jim Cronqvist <jim.cronqvist@gmail.com>
2015-07-17 20:22:44 +0200

    Update Reload_Ticket_List.js

--- Reload_Ticket_List.js
+++ Reload_Ticket_List.js
@@ -29,6 +29,16 @@ jQuery(function()
                 jQuery('#rightCol').children('div.buttons').children('div.pull-right').prepend('<a class="btn btn-info" href="#" style="margin:0px 15px;" onclick="jc_refresh_tickets(); return false;">Reload now</a>');
                 jQuery('#rightCol').children('div.buttons').children('div.pull-right').prepend('<div id="jc_refresh" style="display:inline-block;"></div>');
             }
+            
+            // Set tickets that has the status "On-Hold" to black color in the priority-border.
+            jQuery('li[data-options="#ticket_statuses"] span', jQuery('#ticket-list')).each(function()
+            {
+                if(jQuery(this).html() == 'On-Hold')
+                {
+                    jQuery(this).closest('tr').children('td.priority-border').css('background', 'black');
+                }
+            });
+            
             seconds--;
             jQuery('#jc_refresh').html('Reloading in ' + seconds + ' seconds');
             if(seconds == 0)
